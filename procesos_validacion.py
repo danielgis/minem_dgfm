a675d55de90a7e9da500dbf30e135522ed97f8c8
danielgis <daniel030891@gmail.com>
2020-08-07 15:59:23 -0500

    Se agrego la funcionalidad de tomar en cuenta la zona geografica de acuerdo a la información vectorial del derecho minero

--- procesos_validacion.py
+++ procesos_validacion.py
@@ -4,13 +4,14 @@ import random
 from messages import *
 import os
 
-
 arcpy.env.overwriteOutput = True
 
 # Fields
 _ID = 'ID'
 _CODIGOU = 'codigou'
 _ZONA = 'zona'
+_ZONA_DM = 'zona_dm'
+
 _N_P56 = 'N_P56'
 _E_P56 = 'E_P56'
 _N_W84_1P = 'N_W84_1P'
@@ -25,19 +26,27 @@ PSAD56 = 24860
 WGS84 = 32700
 
 
-def validate_dm(codigou, codigous):
-    if codigou not in codigous:
+def validate_dm(codigou, data):
+    reg = data.loc[data[_CODIGOU] == codigou]
+    if not reg.shape[0]:
         raise RuntimeError("No existe el identificador en la base grafica")
 
 
-def validate_zona(zona):
-    if zona not in (17, 18, 19):
-        raise RuntimeError("El registro no presenta un zona geografica valida")
+def validate_zona(zona, codigou, data):
+    if zona in (17, 18, 19):
+        return zona
+    else:
+        reg = data.loc[data[_CODIGOU] == codigou]
+        zona_dm = int(reg[_ZONA].tolist()[0])
+        if zona_dm in (17, 18, 19):
+            return zona_dm
+        else:
+            raise RuntimeError("El registro no presenta un zona geografica valida")
 
 
 def validate_position(row, x, y, flayer_dm, flayer_ar, wgs=True):
     res = list()
-    zona = row[_ZONA]
+    zona = row[_ZONA_DM]
     codigou = row[_CODIGOU]
     lyr_dm = flayer_dm[zona]
     lyr_ar = flayer_ar[zona]
@@ -79,6 +88,7 @@ def validate_coordenadas(**kwargs):
 
     arcpy.AddMessage(MSG_READ_XLS)
     df = pd.read_excel(kwargs['xls'], converters={_CODIGOU: str})
+    df[_ZONA_DM] = 0
 
     # FeaturesLayers de derechos mineros
     flayer_dm = dict()
@@ -88,22 +98,27 @@ def validate_coordenadas(**kwargs):
     flayer_dm[19] = arcpy.MakeFeatureLayer_management(kwargs['shp_dm_19'], 'feature_dm_19')
 
     arcpy.AddMessage('seleccionando layer aleatoriamente')
-    flayer_selected = random.choice(flayer_dm.keys())
+    flayer_selected = random.choice(flayer_dm.values())
     arcpy.AddMessage('obteniendo codigous {}'.format(flayer_selected))
-    codigous = map(lambda re: re[0], arcpy.da.SearchCursor(flayer_dm[flayer_selected], ['CODIGOU']))
+
+    nparray = arcpy.da.FeatureClassToNumPyArray(flayer_selected, [_CODIGOU, _ZONA])
+    df_dm = pd.DataFrame(nparray)
+
+    # data_dm = map(lambda re: re, arcpy.da.SearchCursor(flayer_dm[flayer_selected], [_CODIGOU, _ZONA]))
+    # codigous = [i[0] for i in data_dm]
 
     arcpy.AddMessage('layer de areas restringidas')
     # FeaturesLayers de areas restringidas
     flayer_ar = dict()
     arcpy.AddMessage('repollo')
-    flayer_ar[17] = arcpy.MakeFeatureLayer_management(kwargs['shp_ar_17'], 'feature_ar_17').getOutput(0)
-    flayer_ar[18] = arcpy.MakeFeatureLayer_management(kwargs['shp_ar_18'], 'feature_ar_18').getOutput(0)
-    flayer_ar[19] = arcpy.MakeFeatureLayer_management(kwargs['shp_ar_19'], 'feature_ar_19').getOutput(0)
+    flayer_ar[17] = arcpy.MakeFeatureLayer_management(kwargs['shp_ar_17'], 'feature_ar_17')
+    flayer_ar[18] = arcpy.MakeFeatureLayer_management(kwargs['shp_ar_18'], 'feature_ar_18')
+    flayer_ar[19] = arcpy.MakeFeatureLayer_management(kwargs['shp_ar_19'], 'feature_ar_19')
 
     arcpy.AddMessage(MSG_EVALUATE_ROWS)
     for i, r in df.iterrows():
         # Habilitar para tets
-        # if i > 20:
+        # if i > 100:
         #     break
         arcpy.AddMessage('{}. [{}] {} {}S'.format(i, r[_ID], r[_CODIGOU], r[_ZONA]))
         res = dict()
@@ -111,8 +126,9 @@ def validate_coordenadas(**kwargs):
         res[_CODIGOU] = r[_CODIGOU]
         res[_ZONA] = r[_ZONA]
         try:
-            validate_dm(r[_CODIGOU], codigous)
-            validate_zona(r[_ZONA])
+            validate_dm(r[_CODIGOU], df_dm)
+            res[_ZONA_DM] = validate_zona(r[_ZONA], r[_CODIGOU], df_dm)
+            r[_ZONA_DM] = res[_ZONA_DM]
             res['P56_DM'], res['P56_AR'] = validate_position(r, r[_E_P56], r[_N_P56], flayer_dm, flayer_ar, wgs=False)
             res['W84_1P_DM'], res['W84_1P_AR'] = validate_position(r, r[_E_W84_1P], r[_N_W84_1P], flayer_dm, flayer_ar)
             res['W84_2P_DM'], res['W84_2P_AR'] = validate_position(r, r[_E_W84_2P], r[_N_W84_2P], flayer_dm, flayer_ar)
